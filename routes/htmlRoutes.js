1a8de6c74f24a87ba291f07c0e3848f559917343
Andrew <andrewy3295@gmail.com>
2023-03-14 21:26:45 -0500

    fixed html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,11 +6,11 @@ const path = require(`path`)
 
 router.get(`/notes`, (req,res) => {
     // what kind of thing do you want to send
-    res.sendFile(path.join(__dirname, `../public/notes.html`))
+    res.sendFile(path.join(__dirname, `../notes.html`))
 })
 
 router.get(`/` , (req,res) => {
-    res.sendFile(path.join(__dirname, `../public/index.html`))
+    res.sendFile(path.join(__dirname, `../index.html`))
 })
 
 module.exports = router;
